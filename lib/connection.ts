fa8e4947d43b0822b43098cee3b99849b4c0010e
Jeff P <yeffrey@gmail.com>
2023-03-05 15:31:50 -0600

    Try railway for database

--- lib/connection.ts
+++ lib/connection.ts
@@ -4,7 +4,7 @@ const client = mysql.createPool(
     process.env.DB_URL ?? {
         user: process.env.DB_USER ?? 'root',
         host: process.env.DB_HOST ?? '127.0.0.1',
-        database: process.env.DB_DATABASE ?? 'singlelink',
+        database: process.env.DB_DATABASE ?? 'railway',
         password: process.env.DB_PASSWORD ?? 'single-my-links',
         port: process.env.DB_PORT ?? 3306
     }
